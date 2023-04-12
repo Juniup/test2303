490497c25e175e80e2448ef73a247dc95ba17fdb
Juniup <junyeob@mobilint.co>
2023-04-12 23:35:01 +0900

    again 0412

--- test2303.py
+++ test2303.py
@@ -1,20 +1,27 @@
 import sys
 
-k, n1 = map(int, sys.stdin.readline().split())
-n2 = k - n1
+N = int(input())
+final_list = []
 
-bi = 1
-
-while True:
-    if k > n1:
-       bi *= k
-       k -= 1
+for i in range(N):
+    a, b = map(int, sys.stdin.readline().split())
     
+    score = 100000000*a + 10*b
+    
+    if b < 0:
+        score += 1
+        
+    final_list.append(score)
+
+final_list.sort()
+
+for i in range(len(final_list)):
+    if final_list[i] % 2 == 0:
+        b1 = (final_list[i] % 100000000) // 10
+        a1 = final_list[i] // 100000000
+        
     else:
-        if n2 > 1:
-            bi //= n2 
-            n2 -= 1
-        else:
-            break
-    
-print(bi)
+        b1 = ((final_list[i] % 100000000)-100000000) // 10
+        a1 = final_list[i] // 100000000 + 1
+
+    print(a1, b1)
